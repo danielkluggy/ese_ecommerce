8ba5b398094d110d54582ed944652db73b481e41
Daniel Larion Klug <dklug22@gmail.com>
2024-09-05 21:57:46 -0300

    init

---
+++ src/main/java/br/udesc/ese_ecommerce/EseEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.udesc.ese_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EseEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EseEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/udesc/ese_ecommerce/EseEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.udesc.ese_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EseEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
